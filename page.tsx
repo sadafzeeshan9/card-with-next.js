f907830831ab6644b8077aa646466bcb16157635
sadafzeeshan9 <160055415+sadafzeeshan9@users.noreply.github.com>
2024-10-07 10:14:35 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,14 +1,34 @@
- import React from "react"
- 
- const Card = (props) => {
-     return(
-        <div>
-            <h1>Entry Cart</h1>
-            <p>Name:{props.name}</p>
-            <p>Roll No:{props.rollno}</p>
-            <p>Day:{props.day}</p>
-        </div>
-     )
- }
+import Card from "@/component/page";
 
- export default Card
+export default function Home(){
+  return(
+
+    <div className="flex flex-wrap justify-center mb-4">
+
+
+    <div  className="max-w-full md:w-1/3 xl:w-1/4 p-6 text-lg bg-amber-300 "> 
+    <Card name="sadaf" rollno={1112} day="monday" />
+
+    </div>
+
+    <hr></hr>
+    
+    <div  className="max-w-full md:w-1/3 xl:w-1/4 p-6 text-lg bg-red-500">
+    <Card name="shazia" rollno={1122} day="monday" />
+    </div>
+   
+   <hr></hr>
+
+
+    <div className="max-w-full md:w-1/3 xl:w-1/4 p-6 text-lg bg-slate-500">
+    <Card name="afsha" rollno={1133} day="monday" />
+     </div>
+
+
+
+
+     
+    </div>
+  )
+}
+
